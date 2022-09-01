cff7c7ab873237b1c20ba151e922dd68ba4cebf5
ArcGate-Manish <manish.beniwal@arcgate.com>
2022-09-01 18:12:35 +0530

    convert to list

--- app/clubs/views.py
+++ app/clubs/views.py
@@ -51,39 +51,39 @@ def get_club_details(id):
 @club_blueprint.route('/list/',methods=['GET'])
 def get_all_club_details():
     club_details = Club.getAllClub()
-    details_list ={}
+    details_list =[]
     if(club_details):
         for row in club_details:
-            details_list[row.id] = {}
-            details_list[row.id]['id'] = row.id
-            details_list[row.id]['name'] = row.name
-            details_list[row.id]['address'] = row.address
-            details_list[row.id]['about'] = row.about
-            details_list[row.id]['email_id'] = row.email_id
-            details_list[row.id]['location'] = row.location
-            details_list[row.id]['district_code'] = row.district_code
-            details_list[row.id]['chartered'] = row.chartered
-            details_list[row.id]['phone_no'] = row.phone_no
-            details_list[row.id]['fax_no'] = row.fax_no
-            details_list[row.id]['vendors'] = row.vendors
-            details_list[row.id]['website'] = row.website
-            details_list[row.id]['rotary_langauge'] = row.rotary_langauge
-            details_list[row.id]['club_ids'] = row.club_ids
-            details_list[row.id]['club_type'] = row.club_type
-            details_list[row.id]['mailing_address'] = row.mailing_address
-            club_members_details = {}
+            details_list.append({})
+            details_list[-1]['id'] = row.id
+            details_list[-1]['name'] = row.name
+            details_list[-1]['address'] = row.address
+            details_list[-1]['about'] = row.about
+            details_list[-1]['email_id'] = row.email_id
+            details_list[-1]['location'] = row.location
+            details_list[-1]['district_code'] = row.district_code
+            details_list[-1]['chartered'] = row.chartered
+            details_list[-1]['phone_no'] = row.phone_no
+            details_list[-1]['fax_no'] = row.fax_no
+            details_list[-1]['vendors'] = row.vendors
+            details_list[-1]['website'] = row.website
+            details_list[-1]['rotary_langauge'] = row.rotary_langauge
+            details_list[-1]['club_ids'] = row.club_ids
+            details_list[-1]['club_type'] = row.club_type
+            details_list[-1]['mailing_address'] = row.mailing_address
+            club_members_details = []
             for club_member in row.club_members:
-                club_members_details[club_member.id] = {}
-                club_members_details[club_member.id]['club_id'] = club_member.club_id
-                club_members_details[club_member.id]['club_member_id'] = club_member.club_member_id
-                club_members_details[club_member.id]['member_first_name'] = club_member.member_first_name
-                club_members_details[club_member.id]['member_last_name'] = club_member.member_last_name
-                club_members_details[club_member.id]['member_from'] = club_member.member_from
-                club_members_details[club_member.id]['profile'] = club_member.profile
-                club_members_details[club_member.id]['club_member_email'] = club_member.club_member_email
-                club_members_details[club_member.id]['member_till'] = club_member.member_till
-                club_members_details[club_member.id]['is_active'] = club_member.club_memb_is_active
-            details_list[row.id]['club_members'] = club_members_details
+                club_members_details.append({})
+                club_members_details[-1]['club_id'] = club_member.club_id
+                club_members_details[-1]['club_member_id'] = club_member.club_member_id
+                club_members_details[-1]['member_first_name'] = club_member.member_first_name
+                club_members_details[-1]['member_last_name'] = club_member.member_last_name
+                club_members_details[-1]['member_from'] = club_member.member_from
+                club_members_details[-1]['profile'] = club_member.profile
+                club_members_details[-1]['club_member_email'] = club_member.club_member_email
+                club_members_details[-1]['member_till'] = club_member.member_till
+                club_members_details[-1]['is_active'] = club_member.club_memb_is_active
+            details_list[-1]['club_members'] = club_members_details
     response = make_response(jsonify(details_list))
     # response.headers['ngrok-skip-browser-warning'] = 'skipbrowser'
     return response
