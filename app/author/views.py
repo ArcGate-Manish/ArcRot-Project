6bf406cc85f3f01b388062e8f1ca1a30cb7b45eb
ArcGate-Manish <manish.beniwal@arcgate.com>
2022-09-05 19:21:53 +0530

    jsonify update

--- app/author/views.py
+++ app/author/views.py
@@ -3,45 +3,30 @@ from . import author_blueprint
 from .models import Author
 
 
-
 # to get details of an author
-@author_blueprint.route('/<int:id>/',methods=['GET'])
+@author_blueprint.route('/<int:id>/', methods=['GET'])
 def get_author_details(id):
     author_details = Author.getAuthorById(id)
-    print(author_details)
-    details_list ={}
-    if(author_details):
-        details_list['author_id'] = author_details.id
-        details_list['description'] = author_details.description
+    details_list = []
+    if (author_details):
+        details_list.append({})
+        details_list[-1]['author_id'] = author_details.id
+        details_list[-1]['description'] = author_details.description
         return jsonify(details_list)
     else:
         return jsonify([])
 
 
-"""@author_blueprint.route('/<int:id>/',methods=['POST'])
-def update_author(id):
-    author = Author.getAuthorById(id)
-    result = {}
-    if(author):
-        data = request.form['description']
-        author.description = data
-        author.update()
-        # status = 200
-        result = {
-            'status' : 'Success'
-        }
-    return make_response(result), 200
-"""
-@author_blueprint.route('/list/',methods=['GET'])
+@author_blueprint.route('/list/', methods=['GET'])
 def get_all_author_details():
     author_details = Author.getAllAuthor()
     print(author_details)
-    details_list ={}
-    if(author_details):
+    details_list = []
+    if (author_details):
         for row in author_details:
-            details_list[row.id] = {}
-            details_list[row.id]['author_id'] = row.id
-            details_list[row.id]['description'] = row.description
+            details_list.append({})
+            details_list[-1]['author_id'] = row.id
+            details_list[-1]['description'] = row.description
         return jsonify(details_list)
     else:
         return jsonify([])
